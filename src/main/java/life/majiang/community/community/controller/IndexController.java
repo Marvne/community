0721db8d7388aae88aab2fb705cff17850e2e3b5
yzp01 <2365702941@qq.com>
2019-09-26 11:34:17 +0800

    add index bootstrp

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -8,10 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 //用于接受前端请求
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name );
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String hello(){
+        return "index";
     }
 }
